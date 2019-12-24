c15ca94e3ef0db2a806227418a39ee635f39b821
Jenny Brennan <jenhbrennan@gmail.com>
2019-12-24 00:02:59 +0000

    Separate out findings.js

---
+++ static/findings.js
@@ -0,0 +1,43 @@
+var quizScoresDiv = document.getElementById('quizScores').getContext('2d');
+var quizScores = new Chart(quizScoresDiv, {
+    type: 'bar',
+    data: {
+        labels: ['0/8', '1/8', '2/8', '3/8', '4/8', '5/8', '6/8', '7/8', '8/8'],
+        datasets: [{
+            label: '# who scored',
+            data: [39, 68, 98, 104, 68, 16, 9, 0 , 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
